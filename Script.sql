587fd80486ac7d031543c135afaf0a48e4d6ae66
HangNT169 <104345338+HangNT169@users.noreply.github.com>
2022-11-14 07:51:56 +0700

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -7,7 +7,7 @@ CREATE DATABASE hibernate_demo;
 USE hibernate_demo;
 -- DROP TABLE hibernate_demo.dbo.category;
 
-CREATE TABLE hibernate_demo.category (
+CREATE TABLE category (
 	id bigint IDENTITY(0,1) NOT NULL,
 	category_code varchar(10)  NULL,
 	category_name nvarchar(50)  NULL,
@@ -21,7 +21,7 @@ CREATE TABLE hibernate_demo.category (
 
 -- DROP TABLE hibernate_demo.dbo.product;
 
-CREATE TABLE hibernate_demo.product (
+CREATE TABLE product (
 	id bigint IDENTITY(0,1) NOT NULL,
 	category_id bigint NULL,
 	product_code varchar(20)  NULL,
@@ -32,22 +32,3 @@ CREATE TABLE hibernate_demo.product (
 	CONSTRAINT product_FK FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
-INSERT INTO hibernate_demo.dbo.category
-(id, category_code, category_name)
-VALUES(0, N'C01', N'Category 1');
-INSERT INTO hibernate_demo.dbo.category
-(id, category_code, category_name)
-VALUES(1, N'C02', N'Category 2');
-INSERT INTO hibernate_demo.dbo.category
-(id, category_code, category_name)
-VALUES(2, N'C03', N'Category 3');
-
-INSERT INTO hibernate_demo.dbo.product
-(id, category_id, product_code, product_name, price, description)
-VALUES(0, 1, N'P01', N'Product 1', 10.0, N'abc');
-INSERT INTO hibernate_demo.dbo.product
-(id, category_id, product_code, product_name, price, description)
-VALUES(1, 2, N'P02', N'Product 2', 11.0, N'hihi');
-INSERT INTO hibernate_demo.dbo.product
-(id, category_id, product_code, product_name, price, description)
-VALUES(2, 1, N'P03', N'Product 3', 22.0, N'test');
